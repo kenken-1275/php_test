9666b8e5de50ed1c8c7507050b139033d1a75237
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-09-15 20:01:20 +0900

    連想配列②

--- asso_array.php
+++ asso_array.php
@@ -11,4 +11,34 @@ echo $array_member["hobby"];
 echo "<pre>";
 var_dump($array_member);
 echo "</pre>";
+
+$array_member_2 = [
+  "1kumi" => [
+    "本田" => [
+      "height" => 170,
+      "hobby" => "サッカー"
+    ],
+    "香川" => [
+      "height" => 165,
+      "hobby" => "サッカー"
+    ]
+  ],
+  "2kumi" => [
+    "長友" => [
+      "height" => 160,
+      "hobby" => "サッカー"
+    ],
+    "乾" => [
+      "height" => 175,
+      "hobby" => "サッカー"
+    ]
+  ],
+];
+
+echo $array_member_2["2kumi"]["長友"]["hobby"];
+
+echo "<pre>";
+var_dump($array_member_2);
+echo "</pre>";
+
 ?>
