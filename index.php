64233434e6e1b4353945206b7131ca0a0f5ab855
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-09-15 18:46:39 +0900

    変数②

--- index.php
+++ index.php
@@ -19,11 +19,14 @@
   echo 123;
 
   echo "<br>";
-
+// 先頭は英字か_ならOK、数字はだめ
   $test = 123;
   $test2 = "テスト";
+  $test3 = 456;
+  $test4 = $test . $test3;
 
-  var_dump($test);
+  // 数字同士だが、ピリオドで結合した場合は、文字列として認識される。
+  var_dump($test4);
   echo "<br>";
 
   var_dump($test2);
