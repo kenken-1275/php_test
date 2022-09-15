de7f38fa50fae6283d3c7a7baffc04365d966293
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-09-15 18:39:35 +0900

    変数練習①

--- index.php
+++ index.php
@@ -18,6 +18,21 @@
 
   echo 123;
 
+  echo "<br>";
+
+  $test = 123;
+  $test2 = "テスト";
+
+  var_dump($test);
+  echo "<br>";
+
+  var_dump($test2);
+  echo "<br>";
+
+  echo $test2;
+
+  // echo $test;
+
 ?>
 
 </body>
