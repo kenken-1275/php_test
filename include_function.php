0f1c6e8c5fe2fb146120a5f878759ab0ccd5f78d
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-09-16 15:45:36 +0900

    include_function②

--- include_function.php
+++ include_function.php
@@ -21,4 +21,26 @@ echo mb_strlen($jap_text);
 $str = "文字列を置換します";
 
 echo str_replace("置換","ちかん",$str);
+
+// 文字列の分割
+$str_2 = "文字列を、分割します";
+echo "<pre>";
+var_dump(explode("、",$str_2));
+echo "</pre>";
+
+// 正規表現
+
+$str_3 = "特定の文字列が含まれるか確認する";
+
+echo preg_match("/文字列/",$str_3);
+
+echo "<br>";
+
+// 指定文字列から文字列を取得する
+
+echo substr("abcde", 2);
+
+echo "<br>";
+
+echo mb_substr("あいうえお",2);
 ?>
