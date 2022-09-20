b0ad8cda3645504a1edd9aee16d1c967f55935e5
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-09-20 19:42:38 +0900

    バリデーションの準備

--- form/input.php
+++ form/input.php
@@ -6,9 +6,9 @@ session_start();
 
 // スーパーグローバル変数 phpでは9種類ある。フォームに入力した内容を連想配列にする。またアドレスに入力した内容が反映される。
 // method"POST"でも同じように連想配列になるが、アドレスに入力内容が表示されなくなる。パスワードとか表示したくない場合はPOSTが良い。
-if(!empty($_SESSION)){
+if(!empty($_POST)){
   echo "<pre>";
-  var_dump($_SESSION);
+  var_dump($_POST);
   echo "</pre>";
 }
 
@@ -21,9 +21,9 @@ function h($str){
 // ②1つのファイル内に、変数を用意して分岐させる。
 $page_flag = 0;
 
-echo "<pre>";
-var_dump($_POST);
-echo "</pre>";
+// echo "<pre>";
+// var_dump($_POST);
+// echo "</pre>";
 
 
 if(!empty($_POST["btn_confirm"])){
@@ -58,6 +58,31 @@ if(!empty($_POST["btn_submit"])){
     <br>
     メールアドレス
     <input type="email" name="email" value="<?php if(!empty($_POST["email"])){ echo h($_POST["email"]);} ?>">
+    <br>
+    ホームページ
+    <input type="url" name="url" value="<?php if(!empty($_POST["url"])){ echo h($_POST["url"]);} ?>">
+    <br>
+    性別
+    <input type="radio" name="gender" value="0">男性
+    <input type="radio" name="gender" value="1">女性
+    <br>
+    年齢
+    <select name="age">
+      <option value="">選択してください</option>
+      <option value="1">〜19歳</option>
+      <option value="2">20歳〜29歳</option>
+      <option value="3">30歳〜39歳</option>
+      <option value="4">40歳〜49歳</option>
+      <option value="5">50歳〜59歳</option>
+      <option value="6">60歳〜</option>
+    </select>
+    <br>
+    お問い合わせ内容
+    <textarea name="contact">
+      <?php if(!empty($_POST["contact"])){ echo h($_POST["contact"]);} ?>
+    </textarea>
+    <br>
+    <input type="checkbox" name="caution" value="1">注意事項にチェックする
     <br>
     <!-- <input type="checkbox" name="sports[]" value="野球">野球
     <input type="checkbox" name="sports[]" value="サッカー">サッカー
