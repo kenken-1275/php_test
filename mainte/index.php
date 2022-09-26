df1e761555b125ae4145c90bce8af3ae2b317189
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-09-26 17:36:40 +0900

    トランザクション

--- mainte/index.php
+++ mainte/index.php
@@ -25,4 +25,21 @@ echo "<pre>";
 var_dump($result);
 echo "</pre>"
 
+// トランザクション まとまった処理 beginTransaction,commit,rollbackという３つのメソッドを使う。
+// ex)銀行 残高を確認->Aさんから引き落とし->Bさんに振り込み　１つでも欠けるとまずいので、何か問題があれば最初に戻る(1連の処理と捉える)。これもtry{}catch(){}を使う。
+// catchの後ろは、ほぼ定型文
+
+$pdo->beginTransaction(); //トランザクションの開始
+
+try{
+  // sql処理
+  $stmt = $pdo->prepare($sql); //プリペアードステートメント
+  $stmt->bindValue(":id",3,PDO::PARAM_INT); //紐付け
+  $stmt->execute(); //実行
+  
+  $pdo->commit(); //まとめて実行
+}catch(PDOException $e){
+  $pdo->rollback(); //更新のキャンセル、PDOExceptionで例外が発生したら、rollbackで処理を巻き戻す。
+}
+
 ?>
