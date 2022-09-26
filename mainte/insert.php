2c2853b49531dc4e8ac2028cff233b373530c2e8
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-09-26 18:17:42 +0900

    DBへの値の保存

---
+++ mainte/insert.php
@@ -0,0 +1,40 @@
+<?php
+
+// DB接続 PDO
+require "db_connection.php";
+// 入力 DB保存 prepare,execute(文字列の場合は、配列にすれば全て処理できるのでbindが必要無くなる)
+
+$params = [
+  "id" => null,
+  "your_name" => "なまえ",
+  "email" => "test@test.com",
+  "url" => "http://test.com",
+  "gender" => "1",
+  "age" => "2",
+  "contact" => "いいい",
+  "created_at" => null
+];
+
+$count = 0;
+$columns = "";
+$values = "";
+
+foreach(array_keys($params) as $key){
+  if($count > 0){
+    $columns .= ",";
+    $values .= ",";
+  }
+  $columns .= $key;
+  $values .= ":" .$key;
+  $count++;
+}
+
+
+$sql = "insert into contacts ($columns)values($values)";
+
+// var_dump($sql);
+// exit;
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute($params); 
+?>
