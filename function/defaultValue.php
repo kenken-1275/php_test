095600936caa96d47473b23a5c61c0e30983a7e1
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-09-27 10:47:26 +0900

    修正

--- function/defaultValue.php
+++ function/defaultValue.php
@@ -1,7 +1,7 @@
 <?php
 
 function defaultValue($string = null){ //引数に = null(or 値) を指定することで、デフォルト値を設定できる。
-  echo $string; . "です";
+  echo $string . "です";
 } 
 
 defaultValue(); //引数なしのこれでも実行できる。ファンクションに引数設定（デフォルト値なし）しているのに、呼び出し時に引数がない場合はエラーが出る。
